16179f6e35927c39c4449e79e4a0ae60445ebef0
danil <danill@intempt.com>
2025-04-02 13:38:10 +0300

    send webhooks

--- app/src/main/java/com/intempt/core/services/firebase/model/PushNotificationContent.kt
+++ app/src/main/java/com/intempt/core/services/firebase/model/PushNotificationContent.kt
@@ -3,5 +3,6 @@ package com.intempt.core.services.firebase.model
 data class PushNotificationContent(
     val title: String,
     val body: String,
-    val image: String
+    val image: String,
+    val webUrl: String
 )
